661e3db6e86a816476583aecd69e087c6016b3e9
Esraa Banat <103361198+EsraaBanat@users.noreply.github.com>
2023-12-21 20:27:50 +0300

    Edit backend url

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
 const nextConfig = {
   async rewrites() {
     const nodeEnv = process.env.NODE_ENV;
-    const destination = "http://localhost:3001/:path*";
-    // const destination = "https://q-pros-assignment.up.railway.app/:path*";
+    // const destination = "http://localhost:3001/:path*";
+    const destination = "https://q-pros-assignment.up.railway.app/:path*";
 
     return [
       {
